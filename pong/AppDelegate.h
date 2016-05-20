1ddcd074b4a54b37c0423ff013ec97430d0110b6
adarsh <adarshsa@thoughtworks.com>
2016-05-20 23:51:03 +0530

    Initial Commit

---
+++ pong/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  pong
+//
+//  Created by adarshks on 5/20/16.
+//  Copyright © 2016 adarshks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
